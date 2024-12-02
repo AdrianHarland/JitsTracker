0380de850cd707afe806cf006698663c9c627061
Adrian Harland <68925121+AdrianHarland@users.noreply.github.com>
2024-12-02 15:29:23 +0000

    MoveBranch push

--- JitsTrackerBE/JitsTrackerBE/obj/Debug/net8.0/JitsTrackerBE.AssemblyInfo.cs
+++ JitsTrackerBE/JitsTrackerBE/obj/Debug/net8.0/JitsTrackerBE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JitsTrackerBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776c5841aee0a24a4b80370750aafd263f037b6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e85cc44b26db28c6cb1ed7aaecbcc43c8ac8e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("JitsTrackerBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JitsTrackerBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
